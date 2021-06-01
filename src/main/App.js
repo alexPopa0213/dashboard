d092ec8a9073276443ceba9af6d031e1871b24e5
alex.popa0213@gmail.com <Seatleonfr1p>
2021-06-01 23:32:34 +0300

    add server state paginate entries if more than 10 hide dummy entry

--- src/main/App.js
+++ src/main/App.js
@@ -8,14 +8,10 @@ function App() {
             <h1>Dashboard</h1>
             <div className="dashboard">
                 <ServerInstance name="1" port="8001"/>
-                <ServerInstance name="2" port="8001"/>
-                <ServerInstance name="3" port="8001"/>
-                <ServerInstance name="4" port="8001"/>
-                <ServerInstance name="5" port="8001"/>
-                <ServerInstance name="6" port="8001"/>
-                <ServerInstance name="7" port="8001"/>
-                <ServerInstance name="8" port="8001"/>
-                <ServerInstance name="9" port="8001"/>
+                <ServerInstance name="2" port="8002"/>
+                <ServerInstance name="3" port="8003"/>
+                <ServerInstance name="4" port="8004"/>
+                <ServerInstance name="5" port="8005"/>
             </div>
         </div>
     );
--- src/server/ServerInstance.js
+++ src/server/ServerInstance.js
@@ -6,30 +6,66 @@ class ServerInstance extends Component {
         super(props);
         this.state = {
             logEntries: [],
+            serverState: "off",
             errorState: true,
         }
     }
 
     render() {
+        let size = this.state.logEntries.length;
+        let dataComponent;
+        if(size > 11){
+           dataComponent = <div className="data">
+                                .....
+                              {this.state.logEntries.slice(Math.max(this.state.logEntries.length - 10, 0))
+                              .map((logEntry) => {
+                                  if(logEntry.term !== 0 && logEntry.cmd !== "")
+                                    {
+                                        return <LogEntry term={logEntry.term} cmd={logEntry.command} index={logEntry.index}/>
+                                    }
+                                 })}
+                           </div>
+        }else{
+            dataComponent = <div className="data">
+                           {this.state.logEntries.map((logEntry) => {
+                            if(logEntry.term !== 0 && logEntry.cmd !== "")
+                               {
+                                  return <LogEntry term={logEntry.term} cmd={logEntry.command} index={logEntry.index}/>
+                               }
+                                 })}
+                         </div>
+        }
+
         return <div className="serverInstance"
                     style={{
-                        backgroundColor: this.state.errorState ? '#ffeff3' : '#e2ffdc',
+                        backgroundColor: this.state.errorState ? '#f2f2f2' : '#e6ffff',
                     }}>
             <h3>server {this.props.name}</h3>
-            <div className="log-size">size: {this.state.logEntries.length}</div>
-            <div className="data">
-                {this.state.logEntries.map((logEntry) => {
-                    return <LogEntry term={logEntry.term} cmd={logEntry.command} index={logEntry.index}/>
-                })}
-            </div>
+            <div className="server-state">state: {this.state.serverState}</div>
+            <div className="log-size">size: {this.state.errorState ? 0 : this.state.logEntries.length - 1}</div>
+            {dataComponent}
         </div>
     }
 
     async componentDidMount() {
-        setInterval(() => this.callAPI(), 100);
+        setInterval(() => this.callGetEntriesEndpoint(), 100);
+        setInterval(() => this.callGetServerStateEndpoint(), 100);
     }
 
-    async callAPI() {
+    async callGetServerStateEndpoint(){
+     let url = 'http://localhost:' + this.props.port + '/api/state';
+     return await fetch(url)
+                .then((res) => res.json())
+                .then((data) => {
+                    this.setState({serverState: data})
+                })
+                .catch(reason => {
+                    this.setState({serverState: "off"})
+                    console.log(reason);
+                });
+    }
+
+    async callGetEntriesEndpoint() {
         let url = 'http://localhost:' + this.props.port + '/api/entries';
         return await fetch(url)
             .then(res => res.json())
